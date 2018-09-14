2d48dc4dcc396534f5b62a461889953be96aca9e
Johnpaul <bohrplus@gmail.com>
2018-09-14 12:25:28 +0100

    [#160495448] Create route for user to post orders
    - Create a data structure (An array) to store the orders
    - Add the array to a model file
    - Create a controller file to push data into the array
      file using es6 functions
    - Create a route file to render the controller
    - Validate with JOI
    - Test Endpoint on Postman

---
+++ src/models/orders.js
@@ -0,0 +1,65 @@
+const orders = [
+{ 
+    id: 1, 
+    type: 'Breakfast', 
+    name: 'Bread with Tea', 
+    orderDate: 'Tuesday, May 11th 2016, 8:30am',
+    expires: "Tuesday, May 11th 2016, 9:10am",
+    quantities: 4,
+    unitPrice: 200,
+    Total: '#800'
+},
+{ 
+    id: 2, 
+    type: 'Breakfast', 
+    name: 'Bread with Tea', 
+    orderDate: 'Tuesday, May 11th 2016, 8:30am',
+    expires: "Tuesday, May 11th 2016, 9:10am",
+    quantities: 4,
+    unitPrice: 200,
+    Total: '#800'
+},
+{ 
+    id: 3, 
+    type: 'Breakfast', 
+    name: 'Bread with Tea', 
+    orderDate: 'Tuesday, May 11th 2016, 8:30am',
+    expires: "Tuesday, May 11th 2016, 9:10am",
+    quantities: 4,
+    unitPrice: 200,
+    Total: '#800'
+},
+{ 
+    id: 4, 
+    type: 'Breakfast', 
+    name: 'Bread with Tea', 
+    orderDate: 'Tuesday, May 11th 2016, 8:30am',
+    expires: "Tuesday, May 11th 2016, 9:10am",
+    quantities: 4,
+    unitPrice: 200,
+    Total: '#800'
+},
+{ 
+    id: 5, 
+    type: 'Breakfast', 
+    name: 'Bread with Tea', 
+    orderDate: 'Tuesday, May 11th 2016, 8:30am',
+    expires: "Tuesday, May 11th 2016, 9:10am",
+    quantities: 4,
+    unitPrice: 200,
+    Total: '#800'
+},
+{ 
+    id: 6, 
+    type: 'Breakfast', 
+    name: 'Bread with Tea', 
+    orderDate: 'Tuesday, May 11th 2016, 8:30am',
+    expires: "Tuesday, May 11th 2016, 9:10am",
+    quantities: 4,
+    unitPrice: 200,
+    Total: '#800'
+},
+];
+
+
+export default orders;
---
+++ src/routes/router.js
@@ -0,0 +1,12 @@
+import express from "express";
+const router = express.Router();
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+import { postOrder } from '../controller/ordersControllers';
+
+//Post /api/v1/orders
+router.post('/orders', postOrder);
+
+export default router;
