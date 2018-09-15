a0fd91b7a1d6ffd7b4f325ab964f770a9c6d4bee
Johnpaul <bohrplus@gmail.com>
2018-09-15 21:41:44 +0100

    [ft-#160532011]create endpoint update order status
    - implement feedback on renaming folders
    - implement feedback on best coding practices
    - create controller action for updating status in order array of objects
    - create route for controller
    - modified .eslintrc.json for best convention with airbnb styleguide

--- src/controller/ordersControllers.js
+++ src/controller/ordersControllers.js
@@ -1,20 +1,24 @@
 import Joi from 'joi';
 import orders from '../models/orders';
 
-export function getAllOrders (req, res) {
-	res.status(200).json({status: 'Success', Orders: orders});
-}
+export const getAllOrders = (req, res) => {
+	res.status(200).json({ status: 'Success', Orders: orders });
+};
 
-export function fetchSingleOrder (req, res) {
+export const fetchSingleOrder = (req, res) => {
 	const order = orders.find( o => o.id === parseInt(req.params.orderId));
-	if(!order) return res.status(404).json({status: 'Failed', message: 'No order with the given id'});
-	res.status(200).json({status: 'success', Order: order});
-}
+	if(!order) return res.status(404)
+		.json({ status: 'Failed', message: 'No order with the given id' });
+		
 
-export function postOrder (req, res) {
-	const {error} = validateOrders(req.body);
+	res.status(200).json({ status: 'success', Order: order });
+};
+
+export const postOrder = (req, res) => {
+	const { error } = validateOrders(req.body);
     
-	if(error) return res.status(400).json({status: 'Failed to create order', message: error.details[0].message});
+	if(error) return res.status(400)
+		.json({ status: 'Failed to create order', message: error.details[0].message });
 
 	const order = {
 		id: orders.length + 1,
@@ -24,15 +28,29 @@ export function postOrder (req, res) {
 		expires: req.body.expires,
 		quantities: req.body.quantities,
 		unitPrice: Math.floor( Math.random() + 200),
-		total: 'N' + req.body.quantities * (Math.floor( Math.random() + 200))
+		total: 'N' + req.body.quantities * (Math.floor( Math.random() + 200)),
+		status: 'pending'
 	};
 
 	orders.push(order);
 
-	res.json({justAdded: order, message: 'order Created'});
-}
+	res.json({ justAdded: order, message: 'order Created' });
+};
 
-function validateOrders(order) {
+export const updateOrders = (req, res) => {
+	const order = orders.find( o => o.id === parseInt(req.params.orderId));
+	if(!order) return res.status(404)
+		.json({ status: 'Failed', message: 'No order with the given id' });
+
+	const { error } = validateStatus(req.body);
+	if(error) return res.status(400)
+		.json({ status: 'Failed to create order', message: error.details[0].message });
+
+	order.status = req.body.status;
+	res.status(200).json({ status: 'success', Order: order });
+};
+
+const validateOrders = (order) => {
 	const schema = {
 		name: Joi.string().min(3).required(),
 		type: Joi.string().min(3).required(),
@@ -43,4 +61,16 @@ function validateOrders(order) {
 
 	return Joi.validate(order, schema);
     
-}
+};
+
+const validateStatus = (orderStatus) => {
+	const schema = {
+		status: Joi.string().min(3).required()
+	};
+
+	return Joi.validate(orderStatus, schema);
+    
+};
+
+
+
