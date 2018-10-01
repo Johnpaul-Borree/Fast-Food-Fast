1b86540b54c92556de5776c8be30d7656680b137
Johnpaul <bohrplus@gmail.com>
2018-10-01 22:01:45 +0100

    [ch-#160873680] Setting up frontend cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 import requestRoutes from './routes/router';
 import apiDocs from './docs/swaggerFood.json';
 
@@ -8,11 +9,23 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocs));
 app.set('json spaces', 40);
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, ' +
+ 'Authorization, Access-Control-Allow-Credentials'
+	);
+	res.header('Access-Control-Allow-Credentials', 'true');
+	next();
+});
 app.get('/', (req, res) => {
 	res.send('Welcome to Fast-Food-Fast');
 });
