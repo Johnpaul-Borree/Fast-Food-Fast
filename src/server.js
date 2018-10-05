f782bcc1d0174b491cfed57842d6ed5f7986cca0
Johnpaul <bohrplus@gmail.com>
2018-10-05 11:46:13 +0100

    heroku fix

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocs));
-app.use(express.static('./public'));
+app.use(express.static('./Public'));
 app.set('json spaces', 40);
 
 app.use((req, res, next) => {
